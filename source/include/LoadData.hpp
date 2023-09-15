568cae707e002b110cdb031d08e04f91d0d30cac
Lien Zhu <lienzhucs@gmail.com>
2023-09-14 18:57:02 -0700

    uses better Makefile

--- source/include/LoadData.hpp
+++ source/include/LoadData.hpp
@@ -5,6 +5,7 @@
 
 #include "global.hpp"
 
+/* NOTE: Row Major storage for matrices probably makes more sense since the files are row major */
 ReturnStatus read_mnist_labels(const std::string&, Eigen::MatrixXd&);
 ReturnStatus read_mnist_images(const std::string&, Eigen::MatrixXd&);
 ReturnStatus load_parameters(const std::string& filename, Eigen::MatrixXd& matrix);
